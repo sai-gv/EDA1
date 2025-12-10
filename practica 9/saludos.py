0d807c8fb164ba4033d314823f15b4a92eb916fe
sai-gv <sebastiangj01@gmail.com>
2025-12-09 22:19:48 -0600

    Update saludos.py

--- practica 9/saludos.py
+++ practica 9/saludos.py
@@ -1,1 +1,8 @@
-
+cadena1='Hola'
+cadena2="Mundo"
+print(cadena1)
+print(cadena2)
+concat_cadenas= cadena1 + cadena2
+print(concat_cadenas)
+num_cadena = concat_cadenas +' '+ str(3)
+print(num_cadena)
