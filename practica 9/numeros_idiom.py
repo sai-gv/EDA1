dd8335a06daa6cb3989dc42ed3c736f740f1d3ec
sai-gv <sebastiangj01@gmail.com>
2025-12-09 22:22:41 -0600

    Create numeros_idiom.py

---
+++ practica 9/numeros_idiom.py
@@ -0,0 +1,7 @@
+def numeros_idiom(num):
+    if num in (1, 2, 3, 4):
+        print("Tu número es {}".format(num))
+    else:
+        print("{} no es una opción".format(num))
+numeros_idiom(3)
+numeros_idiom(7)
