3619b92484c35083c964002eb89e155bf38bc233
sai-gv <sebastiangj01@gmail.com>
2025-12-09 22:23:08 -0600

    Create numeros_switch.py

---
+++ practica 9/numeros_switch.py
@@ -0,0 +1,13 @@
+def numeros(num):
+    if num == 1:
+        print("Tu número es 1")
+    elif num == 2:
+        print("El número es 2")
+    elif num == 3:
+        print("El número es 3")
+    elif num == 4:
+        print("El número es 4")
+    else:
+        print("No hay opción")
+numeros(3)
+numeros(10)
