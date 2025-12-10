123bddbfee6456b29a5bffdb2d9fa8d9ed30310d
sai-gv <sebastiangj01@gmail.com>
2025-12-09 22:22:07 -0600

    Create lista_numeros.py

---
+++ practica 9/lista_numeros.py
@@ -0,0 +1,13 @@
+lista_numeros=[['cero',0],['uno',1,'UNO'],['dos',2],['tres',3],['cuatro',4],['X',5]]
+print(lista_numeros)
+print(lista_numeros[0])
+print(lista_numeros[1])    
+
+print(lista_numeros[2][0])
+print(lista_numeros[2][1])
+               
+print(lista_numeros[1][0])
+print(lista_numeros[1][1])
+print(lista_numeros[1][2])
+print(lista_numeros[5][0])
+print(lista_numeros[5])
