9f0766fc96b835d0b79f3b045a12fbf672191d89
sai-gv <sebastiangj01@gmail.com>
2025-12-09 22:25:49 -0600

    Create tupla_numeros.py

---
+++ practica 9/tupla_numeros.py
@@ -0,0 +1,12 @@
+tupla_numeros=(('cer',0),('uno',1,'UNO'),('dos',2),('tres',3),('cuatro',4),('X',5))
+
+print(tupla_numeros)
+print(tupla_numeros[0])
+print(tupla_numeros[1])
+
+print(tupla_numeros[2][0])
+print(tupla_numeros[2][1])
+
+print(tupla_numeros[1][0])
+print(tupla_numeros[1][1])
+print(tupla_numeros[1][2])
